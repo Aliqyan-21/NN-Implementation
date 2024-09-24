da8272efc46309d2f47e4a524ed02607a1106681
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-09-24 23:47:16 +0530

    feat: data and random numbers

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <random>
+#include <vector>
+
+std::vector<std::pair<int, int>> train_data = {
+    {0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}};
+
+// y = mx + c
+
+float rand_float(void) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<float> dist(0.f, 1.f);
+  return dist(gen);
+}
+
+int main(void) {
+  float x = rand_float();
+  std::cout << x << std::endl;
+
+  for (auto &p : train_data)
+    std::cout << p.first << ", " << p.second << std::endl;
+
+  return 0;
+}
